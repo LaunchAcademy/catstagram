179e190680ad150506d4070d83da3b7d72385189
Eric Kelly <heroiceric@gmail.com>
2014-04-02 10:55:55 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Catstagram::Application.config.secret_key_base = '536a53161595887e277f800304fde302da8ca18ec9023eae37f9adc180615c1d11476c0e676c74cd07fa088cba17ae939c1e4c5952b8603470b56fe5afcef071'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Catstagram::Application.config.session_store :cookie_store, key: '_catstagram_session'

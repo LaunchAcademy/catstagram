3bce0bfe067ef40bdb73c6a34a70cdd80c14bc22
Eric Kelly <heroiceric@gmail.com>
2014-04-03 00:11:19 -0400

    Set up a root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Catstagram::Application.routes.draw do
+  root 'pages#index'
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

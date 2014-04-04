e34b47de9df4b4cc1ec206cd1abbcc58252ebbbd
Eric Kelly <heroiceric@gmail.com>
2014-04-03 22:19:43 -0400

    Update posts factory

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,10 +1,15 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :post do
     association :user
 
-    image "Awesome Image"
-    description "MyText"
+    # You can use the same fixture file that you added earlier
+    image File.open(File.join(Rails.root, '/spec/fixtures/sleeping_cat.jpeg'))
+
+    trait :with_description do
+      # We use sequence to make sure that each post has a unique description.
+      # This makes it easier to test whether or not each post is present on the
+      # page.
+      sequence(:description) { |n| "Meet kitty#{n}. MEOWWW!" }
+    end
   end
 end

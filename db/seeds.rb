5a7dfd1e9a05a3592176ee40ff67a3833f72f084
Eric Kelly <heroiceric@gmail.com>
2014-07-06 14:41:28 -0400

    Move seed images /spec/fixtures -> db/fixtures

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ user = User.create(email: 'seed@example.com', password: 'castagenius')
 
 Dir.foreach('spec/fixtures/') do |file|
   if file.include?('.png')
-   image = File.open(File.join(Rails.root, "/spec/fixtures/#{file}"))
-   Post.create(user: user, image: image)
+    image = File.open(File.join(Rails.root, "/db/fixtures/#{file}"))
+    Post.create(user: user, image: image)
   end
 end
